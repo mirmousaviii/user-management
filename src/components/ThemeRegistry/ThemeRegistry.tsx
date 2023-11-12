f1583ed650c321cb1ed2f912ee073d0044e5b487
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2023-11-12 19:28:21 +0100

    chor: improve ThemeRegistry

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import createCache from '@emotion/cache';
+import createCache, {Options} from '@emotion/cache';
 import {useServerInsertedHTML} from 'next/navigation';
 import {CacheProvider} from '@emotion/react';
 import {ThemeProvider} from '@mui/material/styles';
@@ -10,8 +10,7 @@ import {ColorModeProvider} from "@/contex/color-mode";
 import {darkTheme, lightTheme} from "@/components/ThemeRegistry/theme";
 import {ScopedCssBaseline} from "@mui/material";
 
-export default function ThemeRegistry(props: any) {
-    const {options, children} = props;
+export default function ThemeRegistry({children, options}: { children: React.ReactNode, options:  Options }) {
 
     const [{cache, flush}] = React.useState(() => {
         const cache = createCache(options);
