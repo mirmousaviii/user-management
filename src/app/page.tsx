640d01fcb794f3ecea5d89dd81f9c849b1155d90
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2023-11-13 17:54:57 +0100

    feat: add and delete post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,112 @@
+"use client"
+
+import React, {useEffect} from "react";
 import {Post} from "@/types";
-import {Chip, Typography} from "@mui/material";
+import {Box, Button, Chip, TextField, Typography} from "@mui/material";
 
 async function getPosts() {
-    const res = await fetch(process.env.baseUrl + '/posts')
-    return res.json()
+    const res = await fetch(`${process.env.baseUrl}/posts/`)
+    return res.json();
 }
 
-export default async function HomePage() {
-    const posts: Post[] = await getPosts()
+async function newPost(title: string, body: string) {
+    const res = await fetch(`${process.env.baseUrl}/posts/`, {
+        method: 'POST',
+    });
+
+    return res.json();
+}
+
+async function deletePosts(id: number) {
+    const res = await fetch(`${process.env.baseUrl}/posts/${id}`, {
+        method: 'DELETE',
+    });
+
+    return res.ok;
+}
+
+
+export default function HomePage() {
+    const [posts, setPosts] = React.useState<Post[]>([]);
+    const [title, setTitle] = React.useState<string>('');
+    const [body, setBody] = React.useState<string>('');
+
+    useEffect(() => {
+        getPosts().then((res) => {
+            setPosts(res);
+        });
+    }, []);
+
+
+    const handleNewPost = () => {
+
+        newPost(title, body).then((res) => {
+            if (res) {
+                setPosts(
+                    [...posts, {title, body, id: res.id}]
+                );
+
+                setTitle('');
+                setBody('');
+            }
+        });
+    };
+
+    const handleDelete = (id: number) => {
+        deletePosts(id).then((res) => {
+            if (res) {
+                setPosts(
+                    posts?.filter((post) => post.id !== id)
+                );
+            }
+        });
+    };
+
 
     return (
         <>
-            <Typography variant="h4">Latest Posts:</Typography>
+            <Typography variant="h4">New Post</Typography>
+            <Box component="form"
+                 sx={{
+                     '& > :not(style)': {mb: 1},
+                     mb: 3
+                 }}
+                 noValidate
+                 autoComplete="off"
+            >
+                <TextField label="Title"
+                           variant="outlined"
+                           fullWidth
+                           value={title}
+                           onChange={(e) => setTitle(e.target.value)}
+                />
+                <TextField label="Body"
+                           variant="outlined"
+                           fullWidth
+                           multiline
+                           rows={4}
+                           value={body}
+                           onChange={(e) => setBody(e.target.value)}
+                />
+                <Button variant="contained"
+                        fullWidth
+                        onClick={handleNewPost}
+                        disabled={title === '' || body === ''}
+                >Add new post</Button>
+            </Box>
 
-            {posts?.map((item) => (
+            <Typography variant="h4">Posts</Typography>
+            {posts.map((item) => (
                 <Chip variant="outlined"
                       color="primary"
                       size="small"
-                      sx={{ m: 1 }}
+                      sx={{m: 1}}
                       key={item.id}
                       label={item.title}
+                      onDelete={() => handleDelete(item.id)}
                 />
             ))}
+
         </>
     );
 }
